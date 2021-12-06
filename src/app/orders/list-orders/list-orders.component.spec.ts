dd785a05b647a6a83228edafe575718d4cb90847
Daniel <danfaj@hotmail.com>
2021-12-06 20:10:24 +0100

    initial commit

---
+++ src/app/orders/list-orders/list-orders.component.spec.ts
@@ -0,0 +1,7 @@
+fdescribe("test de órdenes", ()=>{
+it('Primer script de test', ()=>{
+var orders=10*2;
+expect(orders).toBe(20);
+});
+
+});
---
+++ src/app/products/products.service.spec.ts
@@ -0,0 +1,7 @@
+fdescribe("test de productos", ()=>{
+  it('Segundo script de test', ()=>{
+  var productos=50*2;
+  expect(productos).toMatch(/100/);
+  });
+  
+  });
